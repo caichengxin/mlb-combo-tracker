12581bc4ec0ce37cdbc6441adc2a8d606d165f67
caichengxin <chengxin.tsai@gmail.com>
2026-08-18 16:23:24 -0600

    FIXED BUG

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mlb-combo-v40-shell';
+const CACHE_NAME = 'mlb-combo-v41-shell';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', event => {
