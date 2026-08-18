b43eabafdf327683e862104cc061686e56600c1d
caichengxin <chengxin.tsai@gmail.com>
2026-08-18 16:42:24 -0600

    Fixed an OCR Bug

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mlb-combo-v41-shell';
+const CACHE_NAME = 'mlb-combo-v42-shell';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', event => {
